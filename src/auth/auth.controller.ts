2756f2c76ed97a6d44a9e8d8a0d34688ad05f07e
md-sanim-mia <hasansanim562@gmail.com>
2025-12-25 16:32:35 +0600

    nest project setup is done

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+
+
+    authService :AuthService
+
+
+    constructor( authService :AuthService){
+
+        this.authService=authService
+    }
+
+
+    @Post("register") 
+
+    register(){
+
+        const result=this.authService.registerUser()
+        return result
+    }
+    
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+
+    
+    registerUser(){
+
+        return {"message":"User register succesfully !"}
+    }
+
+
+
+    
+}
